16ccb6a9b86f74c07d4e54e6a0fe29fc3eb6e1a4
SHID8118 <shiddharth.shankar8118@gmail.com>
2025-06-22 12:46:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Global Energy Transition Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.sidebar.success("Select a question above.")
