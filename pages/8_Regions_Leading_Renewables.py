5753483247b3417e58b3d18a5cab846b7638a846
SHID8118 <shiddharth.shankar8118@gmail.com>
2025-06-22 22:10:08 +0530

    Add files via upload

---
+++ pages/8_Regions_Leading_Renewables.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Which regions are leaders in renewables adoption?")
+
+st.markdown("""
+This section analyzes trends based on available datasets related to global energy consumption and production.
+More detailed visualizations and insights will be added.
+""")
+
+st.info("👉 Add visualizations and narratives here.")
