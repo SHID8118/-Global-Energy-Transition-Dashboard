6672825f41abfae75e6f7958a693cc2ea29c1810
SHID8118 <shiddharth.shankar8118@gmail.com>
2025-06-22 22:00:35 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Global Energy Transition Dashboard",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
-st.sidebar.success("Select a question above.")
