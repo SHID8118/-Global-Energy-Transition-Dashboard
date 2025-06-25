caa63aa7ed8f18014efd7e38d01951c7edd6c7af
SHID8118 <shiddharth.shankar8118@gmail.com>
2025-06-25 23:05:05 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -33,7 +33,6 @@ page_map = {
     "How have petroleum and liquid fuel production trends changed for individual countries (or globally) from 1973 to 2023?": "4_Petroleum & Liquids Production by Country",
     "How has global energy intensity (measured in MJ per unit GDP PPP) changed over time, and is the world becoming more energy efficient?":"5_Global_Energy_Intensity_vs_GDP.py",
      "Which countries are improving energy efficiency in terms of energy used per unit of economic output (PPP-adjusted)?":"6_Global_Energy_Intensity_vs_GDP_PPP.py",
-
     "Which countries have the highest growth in renewable energy share?": "5_Countries_Growing_Renewables",
     "What is the trend in modern renewables consumption (SDG 7.2 goal)?": "6_SDG72_Trend",
     "How does renewable energy growth correlate with reduction in fossil fuels?": "7_Renewables_vs_Fossil_Reduction",
