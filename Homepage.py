814ed745b727c1a089d468abd6490a9f4974815e
SHID8118 <shiddharth.shankar8118@gmail.com>
2025-06-25 21:11:46 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -5,7 +5,7 @@ st.title("🌍 Global Energy Transition Dashboard")
 st.subheader("Explore energy transition questions based on real-world data")
 
 questions = [
-    "1_global fossil vs renewable trends.py",
+    "1-globally fossil vs renewable trends",
     "Which regions or countries show a consistent decline in oil/gas/coal demand?",
     "How has fossil fuel demand changed globally vs. in specific countries (e.g., India, China, US)?",
     "What sectors are driving fossil fuel reduction (transport, energy, industry)?",
